cba31ef0afbdfdca6f99316fe0993620e19e2167
Wang Yi <wyi743357@gmail.com>
2025-07-19 10:34:22 +0800

    学习

--- bar_descriptions.py
+++ bar_descriptions.py
@@ -1,7 +1,7 @@
-import pygal
+import pygal # A library for creating SVG charts
 from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
-my_style = LS('#333366', base_style=LCS)
+my_style = LS('#333366', base_style=LCS) # Custom style for the chart
 chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
 
 chart.title = 'Python Projects'
